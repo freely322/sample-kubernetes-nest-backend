7f8045b5141779348e8976a87efcdc9b300b36e8
Bohdan <freely994@gmail.com>
2020-07-03 15:05:41 +0300

    Test new version

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `pod ip: ${process.env.POD_IP} time: ${new Date()}`;
+    return `G2O! pod ip: ${process.env.POD_IP}; time: ${new Date()}`;
   }
 }
